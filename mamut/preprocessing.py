4fe3fd15f0d239240d5930c7e13eb7cd24d71b4e
Igor Kołodziej <igor.kolodziej@proton.me>
2025-01-11 13:05:29 +0100

    Preprocessing (#1)
    * Preprocessing
    
    * Compatibility issues

--- mamut/preprocessing.py
+++ mamut/preprocessing.py
@@ -1,293 +1,127 @@
-from typing import List, Literal, Optional, Tuple
+from typing import List, Literal, Optional
 
 import numpy as np
 import pandas as pd
-from scipy.stats import shapiro
+from scipy.stats import skew
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.experimental import enable_iterative_imputer  # noqa
 from sklearn.feature_selection import SelectFromModel
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
+from sklearn.impute import IterativeImputer, KNNImputer, SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, StandardScaler
+from sklearn.preprocessing import (
+    FunctionTransformer,
+    OneHotEncoder,
+    PowerTransformer,
+    RobustScaler,
+)
 
 
-class DataPreprocessor:
+class DataPreprocessor(BaseEstimator, TransformerMixin):
+    imputer_mapping = {
+        "iterative": IterativeImputer,
+        "knn": KNNImputer,
+        "mean": lambda: SimpleImputer(strategy="mean"),
+        "median": lambda: SimpleImputer(strategy="median"),
+        "constant": lambda: SimpleImputer(strategy="constant", fill_value=0),
+    }
+
     def __init__(
         self,
-        numeric_features: List[str],
-        categorical_features: List[str],
-        num_imputation: Literal["mean", "median", "constant"] = "mean",
-        cat_imputation: Literal["most_frequent", "constant"] = "most_frequent",
-        scaling_method: Literal["standard", "minmax", None] = None,
-        test_size: Optional[float] = 0.2,
-        correlation_threshold: float = 0.98,
-        feature_selection: bool = True,
-        feature_selection_treshold: float = 0.8,
+        numeric_features: Optional[List[str]] = None,
+        categorical_features: Optional[List[str]] = None,
+        num_imputation: Literal[
+            "iterative", "mean", "median", "constant"
+        ] = "iterative",
+        cat_imputation: Literal["most_frequent", "constant"] = "constant",
+        feature_selection: bool = False,
+        pca: bool = False,
         random_state: Optional[int] = None,
     ) -> None:
-        """
-        Initialize the DataPreprocessor.
-
-        Parameters:
-        - num_strategy: Strategy for imputing numerical values ('mean', 'median', 'constant').
-        - cat_strategy: Strategy for imputing categorical values ('most_frequent', 'constant').
-        - scaling_method: Scaling method for numerical features ('standard', 'minmax').
-        - test_size: Proportion of the dataset to include in the test split.
-        - random_state: Random state for reproducibility.
-        """
-        self.validate_inputs(num_imputation, cat_imputation, scaling_method)
+        self.numeric_features = numeric_features
+        self.categorical_features = categorical_features
         self.num_imputation = num_imputation
         self.cat_imputation = cat_imputation
-        self.scaling_method = scaling_method
-        self.test_size = test_size
-        self.num_features = numeric_features
-        self.cat_features = categorical_features
-        self.correlation_threshold = correlation_threshold
         self.feature_selection = feature_selection
-        self.feature_selection_treshold = feature_selection_treshold
+        self.pca = pca
         self.random_state = random_state
-        self.fitted_pipeline = None
 
-    @staticmethod
-    def validate_inputs(
-        num_imputation: Literal["mean", "median", "constant"],
-        cat_imputation: Literal["most_frequent", "constant"],
-        scaling_method: Literal["standard", "minmax", None],
-    ) -> None:
-        """
-        Validate the input parameters.
+        self._num_steps = None
+        self._cat_steps = None
+        self._steps = None
+        self.pipeline_ = None
 
-        Parameters:
-        - num_strategy: Strategy for imputing numerical values.
-        - cat_strategy: Strategy for imputing categorical values.
-        - scaling_method: Scaling method for numerical features.
-        - test_size: Proportion of the dataset to include in the test split.
-        - random_state: Random state for reproducibility.
+        if self.num_imputation not in self.imputer_mapping:
+            raise KeyError(f"Invalid num_imputation strategy: {self.num_imputation}")
+        if self.cat_imputation not in ["most_frequent", "constant"]:
+            raise KeyError(f"Invalid cat_imputation strategy: {self.cat_imputation}")
 
-        Raises:
-        - ValueError: If any parameter is invalid.
-        """
-        valid_num_strategies = ["mean", "median", "constant"]
-        valid_cat_strategies = ["most_frequent", "constant"]
-        valid_scaling_methods = ["standard", "minmax", None]
-
-        if num_imputation not in valid_num_strategies:
-            raise ValueError(
-                f"num_strategy must be one of {valid_num_strategies}, got '{num_imputation}'."
-            )
-
-        if cat_imputation not in valid_cat_strategies:
-            raise ValueError(
-                f"cat_strategy must be one of {valid_cat_strategies}, got '{cat_imputation}'."
-            )
-
-        if scaling_method not in valid_scaling_methods:
-            raise ValueError(
-                f"scaling_method must be one of {valid_scaling_methods}, got '{scaling_method}'."
-            )
-
-    @staticmethod
-    def detect_outliers(series: pd.Series) -> bool:
-        """
-        Detect outliers using the IQR method.
-
-        Parameters:
-        - series: Pandas Series to check for outliers.
-
-        Returns:
-        - Boolean indicating whether outliers are detected.
-        """
-        Q1 = series.quantile(0.25)
-        Q3 = series.quantile(0.75)
-        IQR = Q3 - Q1
-        outliers = (series < (Q1 - 1.5 * IQR)) | (series > (Q3 + 1.5 * IQR))
-        return outliers.any()
-
-    @staticmethod
-    def is_gaussian(series: pd.Series, alpha: float = 0.05) -> bool:
-        """
-        Check if a feature follows a Gaussian distribution using the Shapiro-Wilk test.
-
-        Parameters:
-        - series: Pandas Series to test.
-        - alpha: Significance level.
-
-        Returns:
-        - Boolean indicating whether the feature is Gaussian.
-        """
-        stat, p = shapiro(series)
-        return p > alpha
-
-    def choose_scaler(self, X: pd.DataFrame) -> dict:
-        """
-        Choose scalers dynamically based on outlier detection and Gaussian distribution
-        or use the predefined scaling_method if it is set.
-
-        Parameters:
-        - X: Input DataFrame.
-        - num_features: List of numerical feature names.
-
-        Returns:
-        - Dictionary mapping features to scalers.
-        """
-        scalers = {}
-        if self.scaling_method:
-            scaler = (
-                StandardScaler()
-                if self.scaling_method == "standard"
-                else MinMaxScaler()
-            )
-            scalers = {feature: scaler for feature in self.num_features}
-        else:
-            for feature in self.num_features:
-                series = X[feature]
-                has_outliers = self.detect_outliers(series)
-                is_gaussian = self.is_gaussian(series)
-                scalers[feature] = (
-                    StandardScaler() if has_outliers or is_gaussian else MinMaxScaler()
-                )
-        return scalers
-
-    def create_pipeline(self, X: pd.DataFrame) -> Pipeline:
-        """
-        Create a preprocessing pipeline with dynamic scaling for numerical features.
-
-        Parameters:
-        - X: Input DataFrame.
-        - num_features: List of numerical feature names.
-        - cat_features: List of categorical feature names.
-
-        Returns:
-        - A sklearn Pipeline object.
-        """
-        scalers = self.choose_scaler(X)
-        num_transformer_steps = [
-            ("imputer", SimpleImputer(strategy=self.num_imputation))
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
+        self._steps = []
+        self._num_steps = [
+            ("imputer", self.imputer_mapping[self.num_imputation]()),
+            ("power_transformer", FunctionTransformer(self._transform_skewed_features)),
+            ("scaler", RobustScaler()),
         ]
-        for feature in self.num_features:
-            num_transformer_steps.append((f"scaler_{feature}", scalers[feature]))
-
-        num_transformer = Pipeline(num_transformer_steps)
-        cat_transformer = Pipeline(
-            [
-                ("imputer", SimpleImputer(strategy=self.cat_imputation)),
-                ("onehot", OneHotEncoder(handle_unknown="ignore")),
-            ]
-        )
-        preprocessor = ColumnTransformer(
-            [
-                ("num", num_transformer, self.num_features),
-                ("cat", cat_transformer, self.cat_features),
-            ]
-        )
-        return preprocessor
-
-    def remove_highly_correlated_features(
-        self, X: pd.DataFrame
-    ) -> Tuple[pd.DataFrame, List[str]]:
-        """
-        Remove one column from each pair of highly correlated features.
-
-        Parameters:
-        - X: Input DataFrame with numerical features.
-        - threshold: Correlation threshold above which features are considered highly correlated.
-
-        Returns:
-        - DataFrame with highly correlated features removed.
-        """
-        X_copy = X[self.num_features].copy()
-        corr_matrix = X_copy.corr().abs()
-        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
-        to_drop = [
-            column
-            for column in upper.columns
-            if any(upper[column] > self.correlation_threshold)
-        ]
-        return X.drop(columns=to_drop), [
-            feature for feature in self.num_features if feature not in to_drop
+        self._cat_steps = [
+            ("imputer", SimpleImputer(strategy=self.cat_imputation)),
+            ("onehot", OneHotEncoder(drop="first", handle_unknown="error")),
         ]
 
-    def select_important_features(
-        self, X_train: np.ndarray, X_test: np.ndarray, y_train: pd.Series
-    ) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Select the top `threshold_percent` most important features using a RandomForestClassifier
-        after the data has been transformed.
+        if not self.numeric_features:
+            self.numeric_features = X.select_dtypes(include="number").columns.tolist()
+        if not self.categorical_features:
+            self.categorical_features = X.select_dtypes(
+                exclude="number"
+            ).columns.tolist()
 
-        Parameters:
-        - X_train_transformed: Transformed training data.
-        - X_test_transformed: Transformed test data.
-        - y_train: Target variable for training.
-        - threshold_percent: Proportion of features to retain (default is 80%).
-
-        Returns:
-        - X_train_reduced: Training data with selected features.
-        - X_test_reduced: Test data with selected features.
-        - selected_features: List of selected feature names.
-        """
-        rf = RandomForestClassifier(random_state=self.random_state)
-        rf.fit(X_train, y_train.values.ravel())
-
-        importances = rf.feature_importances_
-        num_features_to_select = int(len(importances) * self.feature_selection_treshold)
-
-        selector = SelectFromModel(
-            rf, max_features=num_features_to_select, threshold=-np.inf
+        self._steps.append(
+            (
+                "clean",
+                ColumnTransformer(
+                    [
+                        ("num", Pipeline(self._num_steps), self.numeric_features),
+                        ("cat", Pipeline(self._cat_steps), self.categorical_features),
+                    ]
+                ),
+            )
         )
-        selector.fit(X_train, y_train.values.ravel())
-
-        selected_columns = selector.get_support(indices=True)
-        X_train = X_train[:, selected_columns]
-        X_test = X_test[:, selected_columns]
-
-        # selected_feature_names = [f"feature_{i}" for i in selected_columns]
-        return X_train, X_test
-
-    def fit_transform(
-        self, X: pd.DataFrame, y: pd.Series
-    ) -> Tuple[np.ndarray, pd.Series, np.ndarray, pd.Series]:
-        """
-        Split, fit, and transform the data.
-
-        Parameters:
-        - X: Input DataFrame.
-        - y: Target Series or DataFrame.
-
-        Returns:
-        - X_train, X_test, y_train, y_test: Transformed and split data.
-        """
-        if not self.num_features:
-            self.num_features = X.select_dtypes(include=["number"]).columns
-        if not self.cat_features:
-            self.cat_features = X.select_dtypes(exclude=["number"]).columns
-
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=self.test_size, random_state=self.random_state
-        )
-        X_train, self.num_features = self.remove_highly_correlated_features(X_train)
-        self.fitted_pipeline = self.create_pipeline(X_train)
-        X_train = self.fitted_pipeline.fit_transform(X_train)
-        X_test = self.fitted_pipeline.transform(X_test)
 
         if self.feature_selection:
-            X_train, X_test = self.select_important_features(X_train, X_test, y_train)
-
-        return X_train, y_train, X_test, y_test
-
-    def transform(self, X: pd.DataFrame) -> np.ndarray:
-        """
-        Transform the data (after fitting).
-
-        Parameters:
-        - X: Input DataFrame.
-
-        Returns:
-        - Transformed data.
-        """
-        if self.fitted_pipeline is None:
-            raise ValueError(
-                "Pipeline has not been fitted. Call 'fit_transform' first."
+            self._steps.append(
+                (
+                    "feature_selection",
+                    SelectFromModel(
+                        ExtraTreesClassifier(random_state=self.random_state)
+                    ),
+                )
             )
 
-        return self.fitted_pipeline.transform(X)
+        if self.pca:
+            self._steps.append(("pca", PCA(n_components=0.9, svd_solver="full")))
+
+        self.pipeline_ = Pipeline(self._steps)
+        self.pipeline_.fit(X, y)
+
+    def transform(self, X: pd.DataFrame, y: pd.Series) -> (np.ndarray, np.ndarray):
+        if not self.pipeline_:
+            raise RuntimeError("The pipeline has not been fitted yet.")
+        return self.pipeline_.transform(X)
+
+    def fit_transform(self, X: pd.DataFrame, y: pd.Series) -> (np.ndarray, np.ndarray):
+        self.fit(X, y)
+        return self.transform(X, y)
+
+    @staticmethod
+    def _transform_skewed_features(X: np.ndarray) -> np.ndarray:
+        pt = PowerTransformer(method="yeo-johnson", standardize=False)
+        for i in range(X.shape[1]):
+            feature = X[:, i]
+            feature_skewness = skew(feature)
+            if abs(feature_skewness) > 0.5:
+                feature_reshaped = feature.reshape(-1, 1)
+                transformed_feature = pt.fit_transform(feature_reshaped)
+                X[:, i] = transformed_feature.flatten()
+        return X
--- mamut/wrapper.py
+++ mamut/wrapper.py
@@ -1,6 +1,11 @@
 import logging
+import os
 
+import joblib
+import pandas as pd
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 
 from .evaluation import ModelEvaluator
 from .model_selection import ModelSelector
@@ -10,35 +15,54 @@ log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-class PackageName:
+class Mamut:
     def __init__(self, preprocess: bool = True, **preprocessor_kwargs):
         self.preprocess = preprocess
-        # self.preprocess_args =
-        self.preprocessor = (
-            DataPreprocessor(**preprocessor_kwargs) if preprocess else None
-        )
-        self.model_selector = None
-        # self.model_selector_kwargs = model_selector_kwargs
 
-    def fit(self, X, y):
         if self.preprocess:
-            X_train, y_train, X_test, y_test = self.preprocessor.fit_transform(X, y)
-        else:
-            X_train, y_train, X_test, y_test = train_test_split(
-                X, y, test_size=0.2, random_state=42
+            self.preprocessor = (
+                DataPreprocessor(**preprocessor_kwargs) if preprocess else None
             )
+        self.model_selector = None
+        self.fitted_models_ = None
+        self.cv_scores_ = None
+        self.results_df_ = None
 
-        self.model_selector = ModelSelector(X_train, y_train, X_test, y_test)
-        best_model, best_params, best_score, fitted_models = (
-            self.model_selector.compare_models()
+    def fit(self, X: pd.DataFrame, y: pd.DataFrame):
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, stratify=y
         )
 
-        log.info(f"Best model: {best_model.__class__.__name__}")
-        log.info(f"Best parameters: {best_params}")
-        log.info(f"Best score: {best_score:.4f}")
+        if self.preprocess:
+            X_train = self.preprocessor.fit_transform(X_train, y_train)
+
+        self.model_selector = ModelSelector(X_train, y_train)
+        fitted_models, cv_scores = self.model_selector.compare_models()
+
+        self.fitted_models_ = [
+            Pipeline([("preprocessor", self.preprocessor), ("model", model)])
+            for model in fitted_models
+        ]
+        self.cv_scores_ = cv_scores
+
+        best_model = fitted_models[0]
+
+        y_pred = best_model.predict(X_test)
+        test_score = accuracy_score(y_test, y_pred)
+
+        log.info(f"Best model: {best_model.named_steps['model'].__class__.__name__}")
+        log.info(f"Best score: {test_score:.4f}")
 
         evaluator = ModelEvaluator(fitted_models, X_test, y_test)
-        results_df = evaluator.evaluate()
+        self.results_df_ = evaluator.evaluate()
         evaluator.plot_results()
 
-        return best_model, best_params, results_df
+        models_dir = "fitted_models"
+        os.makedirs(models_dir, exist_ok=True)
+        for model in self.fitted_models_:
+            model_name = model.named_steps["model"].__class__.__name__
+            model_path = os.path.join(models_dir, f"{model_name}.joblib")
+            joblib.dump(model, model_path)
+            log.info(f"Saved model {model_name} to {model_path}")
+
+        return best_model
