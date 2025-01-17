d0664601fa7158e0f05d244a64d315ed3dd854b9
Igor Kolodziej <igor.kolodziej@proton.me>
2025-01-17 10:37:41 +0100

    Hotfix

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -12,3 +12,16 @@ def test_preprocessor_binary_y(X, binary_y):
     assert isinstance(Xft, np.ndarray)
     assert isinstance(yft, np.ndarray)
     assert isinstance(Xt, np.ndarray)
+
+
+def test_subsequent_transform_calls(X, binary_y):
+    prp = Preprocessor()
+    Xft, yft = prp.fit_transform(X, binary_y)
+    Xt = prp.transform(X)
+    Xt2 = prp.transform(X)
+
+    assert (Xt == Xt2).all().all()
+    assert isinstance(Xt2, np.ndarray)
+    assert isinstance(Xft, np.ndarray)
+    assert isinstance(yft, np.ndarray)
+    assert isinstance(Xt, np.ndarray)
